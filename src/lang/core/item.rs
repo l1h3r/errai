b7da6d8fb5414a991196e93954a662c0743a3235
l1h3r <5410284+l1h3r@users.noreply.github.com>
2026-01-09 19:27:15 -0800

    Term/Item enhancements
    - Added `Sync` bound to `Item`.
    - Added `Item::{as_any,as_mut_any,into_any}` helper functions.
    - Added `Term` downcast helper functions.

--- src/lang/core/item.rs
+++ src/lang/core/item.rs
@@ -3,6 +3,28 @@ use std::any::Any;
 use std::fmt::Debug;
 use std::fmt::Display;
 
-pub trait Item: Any + Debug + Display + DynClone + Send + 'static {}
+pub trait Item: Any + Debug + Display + DynClone + Send + Sync + 'static {
+  fn as_any(&self) -> &(dyn Any + Send + Sync);
+  fn as_mut_any(&mut self) -> &mut (dyn Any + Send + Sync);
+  fn into_any(self: Box<Self>) -> Box<dyn Any + Send + Sync>;
+}
 
-impl<T> Item for T where T: Any + Debug + Display + DynClone + Send + 'static {}
+impl<T> Item for T
+where
+  T: Any + Debug + Display + DynClone + Send + Sync + 'static,
+{
+  #[inline]
+  fn as_any(&self) -> &(dyn Any + Send + Sync) {
+    self
+  }
+
+  #[inline]
+  fn as_mut_any(&mut self) -> &mut (dyn Any + Send + Sync) {
+    self
+  }
+
+  #[inline]
+  fn into_any(self: Box<Self>) -> Box<dyn Any + Send + Sync> {
+    self
+  }
+}
--- src/lang/core/term.rs
+++ src/lang/core/term.rs
@@ -1,4 +1,5 @@
 use dyn_clone::clone_box;
+use std::any::Any;
 use std::fmt::Debug;
 use std::fmt::Display;
 use std::fmt::Formatter;
@@ -8,34 +9,94 @@ use crate::lang::Item;
 
 /// A boxed piece of arbitrary data.
 #[repr(transparent)]
-pub struct Term(Box<dyn Item>);
+pub struct Term {
+  data: Box<dyn Item>,
+}
 
 impl Term {
   /// Creates a new `Term`.
   #[inline]
-  pub fn new<T>(value: T) -> Self
+  pub fn new<T>(data: T) -> Self
   where
     T: Item,
   {
-    Self(Box::new(value))
+    Self {
+      data: Box::new(data),
+    }
+  }
+
+  /// Creates a new `Term` from the given `error`.
+  #[inline]
+  pub(crate) fn new_error(error: Box<dyn Any + Send>) -> Self {
+    match error.downcast::<&str>() {
+      Ok(error) => Self::new(error),
+      Err(error) => match error.downcast::<String>() {
+        Ok(error) => Self::new(error),
+        Err(error) => Self::new(format!("{error:?}")),
+      },
+    }
+  }
+
+  /// Returns `true` if the inner type is the same as `T`.
+  #[inline]
+  pub fn is<T>(&self) -> bool
+  where
+    T: 'static,
+  {
+    self.data.as_any().is::<T>()
+  }
+
+  /// Returns some reference to the inner value if it is of type `T`, or `None` if it isn't.
+  #[inline]
+  pub fn downcast_ref<T>(&self) -> Option<&T>
+  where
+    T: 'static,
+  {
+    self.data.as_any().downcast_ref()
+  }
+
+  /// Returns some mutable reference to the inner value if it is of type `T`, or `None` if it isn't.
+  #[inline]
+  pub fn downcast_mut<T>(&mut self) -> Option<&mut T>
+  where
+    T: 'static,
+  {
+    self.data.as_mut_any().downcast_mut()
+  }
+
+  /// Downcasts the boxed term to a concrete type.
+  ///
+  /// Safety
+  ///
+  /// The contained value must be of type `T`. Calling this method with the
+  /// incorrect type is undefined behavior.
+  #[inline]
+  pub unsafe fn downcast_unchecked<T>(self) -> Box<T>
+  where
+    T: 'static,
+  {
+    // SAFETY: This is guaranteed to be safe by the caller.
+    unsafe { Box::from_raw(Box::into_raw(self.data).cast::<T>()) }
   }
 }
 
 impl Clone for Term {
   #[inline]
   fn clone(&self) -> Self {
-    Self(clone_box(&*self.0))
+    Self {
+      data: clone_box(&*self.data),
+    }
   }
 }
 
 impl Debug for Term {
   fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-    write!(f, "Term({:?})", self.0)
+    write!(f, "Term({:?})", self.data)
   }
 }
 
 impl Display for Term {
   fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-    write!(f, "Term({})", self.0)
+    write!(f, "Term({})", self.data)
   }
 }
