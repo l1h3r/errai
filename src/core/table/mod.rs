5c6dbba999e0bd39d0f0734ff7d8611cda905ca0
l1h3r <5410284+l1h3r@users.noreply.github.com>
2026-01-24 16:36:54 -0800

    Purge everything; prepare for v2

--- src/core/table/mod.rs
+++
@@ -1,8 +0,0 @@
-mod atom_table;
-mod proc_table;
-
-pub use atom_table::AtomTable;
-pub use atom_table::AtomTableError;
-pub use proc_table::ProcTable;
-pub use proc_table::ProcTableFull;
-pub use proc_table::ProcTableKeys;
--- src/utils/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Utility types and functions used throughout the runtime.
-//!
-//! # Contents
-//!
-//! - [`CatchUnwind`]: Future wrapper for catching panics
-
-mod futures;
-
-pub(crate) use self::futures::CatchUnwind;
