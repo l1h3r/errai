3efb3aa534fbacddd8a53081a6830c25135212b8
Devin Turner <5410284+l1h3r@users.noreply.github.com>
2026-03-17 17:56:32 +0000

    Add `ProcDict` type (#12)

---
+++ src/erts/mod.rs
@@ -0,0 +1,5 @@
+//! Core ERTS (Errai Runtime System) types and APIs.
+
+mod state;
+
+pub(crate) use self::state::ProcDict;
---
+++ src/erts/state/mod.rs
@@ -0,0 +1,3 @@
+mod proc_dict;
+
+pub(crate) use self::proc_dict::ProcDict;
